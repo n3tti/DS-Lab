e8a85bff430bbe17f9a7c77727f35db7ff687479
mrpetrkol <mrpetrkol2@gmail.com>
2024-11-09 01:39:16 +0100

    clean main

--- app/main.py
+++ app/main.py
@@ -9,15 +9,3 @@ if __name__ == "__main__":
     process = CrawlerProcess(get_project_settings())
     process.crawl(CrawlingSpider)
     process.start()
-
-
-
-# from app.repository.db import db
-
-
-# if __name__ == '__main__':
-#     db.create_user(name="Alice", email="alice@example.com")
-#     user = db.get_user(name="Alice")
-    
-#     print(user)
-#     print()
