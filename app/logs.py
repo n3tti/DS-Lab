dfd21ee2eb11e8c6cb50daa0f0bb7198554e4f70
mrpetrkol2 <mrpetrkol2@gmail.com>
2024-11-17 05:37:42 +0100

    temp: working kindof

--- app/logs.py
+++ app/logs.py
@@ -2,17 +2,17 @@ import logging
 import structlog
 import sys
 
-logging.basicConfig(
-    stream=sys.stdout,
-    format="%(message)s",
-    level=logging.INFO,
-)
-logging.disable(logging.DEBUG)
+
+import logging
+import structlog
+
+
 
 def uppercase_log_level(logger, log_method, event_dict):
     if 'level' in event_dict:
         event_dict['level'] = event_dict['level'].upper()
     return event_dict
+
 
 
 shared_processors = [
@@ -29,11 +29,14 @@ shared_processors = [
     structlog.contextvars.merge_contextvars,
 ]
 
+
+
+
 if sys.stderr.isatty():
-    processors = shared_processors
+    shared_processors = shared_processors
     rendering_processor = structlog.dev.ConsoleRenderer()
 else:
-    processors = shared_processors + [
+    shared_processors = shared_processors + [
         uppercase_log_level,
         structlog.processors.dict_tracebacks,
         structlog.processors.EventRenamer("message"),
@@ -41,42 +44,21 @@ else:
     rendering_processor = structlog.processors.JSONRenderer()
 
 
-# ##################################################
+
 structlog.configure(
     processors=[
-        # structlog.stdlib.filter_by_level,
-        *processors,
-        rendering_processor,
-        # structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        # Prepare event dict for `ProcessorFormatter`.
+        *shared_processors,
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
-    context_class=dict,
     logger_factory=structlog.stdlib.LoggerFactory(),
-    wrapper_class=structlog.stdlib.BoundLogger,
-    cache_logger_on_first_use=True,
 )
 
-# #################################################
-
-
-# Adapt logging to use the same configuration as structlog
-
-# formatter = structlog.stdlib.ProcessorFormatter(
-#     # These run ONLY on `logging` entries that do NOT originate within
-#     # structlog.
-#     foreign_pre_chain=shared_processors,
-#     # These run on ALL entries after the pre_chain is done.
-#     processors=[
-#         # Remove _record & _from_structlog.
-#         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-#         structlog.dev.ConsoleRenderer(),
-#     ],
-# )
-
 formatter = structlog.stdlib.ProcessorFormatter(
-    foreign_pre_chain=processors,
+    foreign_pre_chain=shared_processors,
     processors=[
         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-        rendering_processor,
+        structlog.dev.ConsoleRenderer(),
     ],
 )
 
@@ -84,21 +66,107 @@ handler = logging.StreamHandler()
 # Use OUR `ProcessorFormatter` to format all `logging` entries.
 handler.setFormatter(formatter)
 root_logger = logging.getLogger()
-if (root_logger.hasHandlers()):
-    root_logger.handlers.clear()
 root_logger.addHandler(handler)
-# root_logger.setLevel(logging.INFO)
-# root_logger.propagate = False
+root_logger.setLevel(logging.INFO)
 
 
-# # logging.basicConfig(level=logging.ERROR, format='%(message)s')
+# logging.basicConfig(
+#     stream=sys.stdout,
+#     format="%(message)s",
+#     level=logging.INFO,
+# )
+# logging.disable(logging.DEBUG)
 
-# ##################################################
+# def uppercase_log_level(logger, log_method, event_dict):
+#     if 'level' in event_dict:
+#         event_dict['level'] = event_dict['level'].upper()
+#     return event_dict
 
-# # structlog.configure(
-# #     wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+
+# shared_processors = [
+#     # Processors that have nothing to do with output,
+#     # e.g., add timestamps or log level names.
+
+#     # structlog.stdlib.filter_by_level,
+#     structlog.stdlib.add_log_level,
+#     structlog.stdlib.add_logger_name,
+#     structlog.processors.TimeStamper(fmt="iso"),
+#     structlog.processors.ExceptionPrettyPrinter(),
+#     structlog.processors.StackInfoRenderer(),
+#     structlog.processors.format_exc_info,
+#     structlog.contextvars.merge_contextvars,
+# ]
+
+# if sys.stderr.isatty():
+#     processors = shared_processors
+#     rendering_processor = structlog.dev.ConsoleRenderer()
+# else:
+#     processors = shared_processors + [
+#         uppercase_log_level,
+#         structlog.processors.dict_tracebacks,
+#         structlog.processors.EventRenamer("message"),
+#     ]
+#     rendering_processor = structlog.processors.JSONRenderer()
+
+
+# # ##################################################
+# structlog.configure(
+#     processors=[
+#         # structlog.stdlib.filter_by_level,
+#         *processors,
+#         rendering_processor,
+#         # structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+#     ],
+#     context_class=dict,
+#     logger_factory=structlog.stdlib.LoggerFactory(),
+#     wrapper_class=structlog.stdlib.BoundLogger,
+#     cache_logger_on_first_use=True,
+# )
+
+# # #################################################
+
+
+# # Adapt logging to use the same configuration as structlog
+
+# # formatter = structlog.stdlib.ProcessorFormatter(
+# #     # These run ONLY on `logging` entries that do NOT originate within
+# #     # structlog.
+# #     foreign_pre_chain=shared_processors,
+# #     # These run on ALL entries after the pre_chain is done.
+# #     processors=[
+# #         # Remove _record & _from_structlog.
+# #         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+# #         structlog.dev.ConsoleRenderer(),
+# #     ],
 # # )
-# ##################################################
+
+# formatter = structlog.stdlib.ProcessorFormatter(
+#     foreign_pre_chain=processors,
+#     processors=[
+#         structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+#         rendering_processor,
+#     ],
+# )
+
+# handler = logging.StreamHandler()
+# # Use OUR `ProcessorFormatter` to format all `logging` entries.
+# handler.setFormatter(formatter)
+# root_logger = logging.getLogger()
+# if (root_logger.hasHandlers()):
+#     root_logger.handlers.clear()
+# root_logger.addHandler(handler)
+# # root_logger.setLevel(logging.INFO)
+# # root_logger.propagate = False
+
+
+# # # logging.basicConfig(level=logging.ERROR, format='%(message)s')
+
+# # ##################################################
+
+# # # structlog.configure(
+# # #     wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+# # # )
+# # ##################################################
 
 
 
