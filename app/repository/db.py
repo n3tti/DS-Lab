ab730d80f36e2aeb04c8b87f05caabc2d837d6e4
mrpetrkol2 <mrpetrkol2@gmail.com>
2024-11-22 21:40:26 +0100

    migration: add markdown_content back to scraped_pages

--- app/repository/db.py
+++ app/repository/db.py
@@ -34,20 +34,42 @@ class Database:
             session.flush()
             return scraped_page.model_copy(deep=True)
 
-    def update_scraped_page_status(self, scraped_page_id: int, status: PageStatusEnum):
+    def update_scraped_page_status(self, scraped_page_id: int, status: PageStatusEnum) -> None:
         with session_scope() as session:
             scraped_page_obj = session.query(ScrapedPage).filter(ScrapedPage.id == scraped_page_id).first()
             if scraped_page_obj is None:
                 return None
             scraped_page_obj.status = status
 
-    def get_scraped_page(self, url) -> ScrapedPage | None:
+    def get_scraped_page(self, url: str) -> ScrapedPage | None:
         with session_scope() as session:
             scraped_page_obj = session.query(ScrapedPage).filter(ScrapedPage.url == url).first()
             if scraped_page_obj is None:
                 return None
 
             return scraped_page_obj.model_copy(deep=True)
+
+    def get_scraped_page_by_id(self, scraped_page_id: int) -> ScrapedPage | None:
+        with session_scope() as session:
+            scraped_page_obj = session.query(ScrapedPage).filter(ScrapedPage.id == scraped_page_id).first()
+            if scraped_page_obj is None:
+                return None
+
+            return scraped_page_obj.model_copy(deep=True)
+
+    def get_scraped_page_unconverted_ids(self):
+        with session_scope() as session:
+            query = session.query(ScrapedPage.id).filter(ScrapedPage.status != PageStatusEnum.FINALIZED).order_by(ScrapedPage.id).yield_per(1000)
+            for (page_id,) in query:
+                yield page_id
+
+    def update_scraped_page_with_markdown_content(self, scraped_page_id: int, markdown_content: str) -> None:
+        with session_scope() as session:
+            scraped_page_obj = session.query(ScrapedPage).filter(ScrapedPage.id == scraped_page_id).first()
+            if scraped_page_obj is None:
+                return None
+            scraped_page_obj.content_formatted_with_markdown = markdown_content
+            scraped_page_obj.status = PageStatusEnum.FINALIZED
 
 
 db = Database()
--- scripts/process_html_to_markdown.py
+++ scripts/process_html_to_markdown.py
@@ -1,26 +1,21 @@
 import html2text
-import requests
 
 from app.logs import logger
+from app.repository.db import db
 
 if __name__ == "__main__":
-    logger.debug("This is a debug message: Processing pdfs... 1")
-    logger.info("This is an info message: Processing pdfs... 2")
+    for unconverted_scraped_page_id in db.get_scraped_page_unconverted_ids():
+        logger.info(f"Converting scraped page with id: {unconverted_scraped_page_id} to MarkDown")
 
-    url = "https://www.efv.admin.ch/efv/fr/home/efv/erechnung/aktuell.html"
-    response = requests.get(url)
+        unconverted_scraped_page = db.get_scraped_page_by_id(unconverted_scraped_page_id)
 
-    html_content = response.text
+        html_content = unconverted_scraped_page.response_text
 
-    h = html2text.HTML2Text()
+        h = html2text.HTML2Text()
 
-    # Ignore links and images if desired
-    h.ignore_links = True
-    h.ignore_images = True
+        # h.ignore_links = True
+        h.ignore_images = True
 
-    # Convert to Markdown
-    markdown_content = h.handle(html_content)
+        markdown_content = h.handle(html_content)
 
-    # Save to a Markdown file
-    with open("output.md", "w", encoding="utf-8") as md_file:
-        md_file.write(markdown_content)
+        db.update_scraped_page_with_markdown_content(unconverted_scraped_page_id, markdown_content)
