b3b21ed9ed7e5d5b4addee35c333dbdd2d4b77f1
mrpetrkol2 <mrpetrkol2@gmail.com>
2024-12-09 21:30:55 +0100

    clean repository a bit

--- app/html2md/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.repository.db import db
-
-from .converter import HTMLToMarkdownConverter
-
-
-def run_conversion():
-    converter = HTMLToMarkdownConverter(db)
-    converter.process_all_pages()
