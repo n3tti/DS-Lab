08baff87baeff159376db0b397af2c3a5fd3636e
mrpetrkol2 <mrpetrkol2@gmail.com>
2024-12-20 17:26:13 +0100

    fix models.py after merging

--- app/repository/models.py
+++ app/repository/models.py
@@ -26,7 +26,6 @@ class PageStatusEnum(str, Enum):
     COMPLETED = "Completed"  # completed crawling but not converted to Markdown
     FAILED = "Failed"
     REVISITED = "Revisited"
-    FINALIZED = "Finalized"  # goes after the status "Completed"
 
 
 class LinkStatusEnum(str, Enum):
